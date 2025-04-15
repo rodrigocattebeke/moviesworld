ed3d52caa17f0426d360d70f9c8992311abcfaf9
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-04-15 15:55:42 -0300

    refactor(Buscar page): Transform Buscar page to client component

---
+++ src/components/pages/Buscar/Buscar.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { notFound, useSearchParams } from "next/navigation";
+import styles from "./buscar.module.css";
+import { useEffect, useState } from "react";
+import { MovieList } from "@/components/movie/MovieList/MovieList";
+
+export default function Buscar() {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("q");
+  const [results, setResults] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+
+  if (!query) return notFound();
+
+  useEffect(() => {
+    const getResults = async () => {
+      setIsLoading(true);
+      const res = await fetch(`/api/buscar?q=${query}&page=${page}`);
+      const results = await res.json();
+      setResults(results.results);
+      setIsLoading(false);
+    };
+    getResults();
+  }, [query]);
+
+  return (
+    <section className={`container position-relative mt-3`}>
+      <div className={styles.container}>
+        <div className="container d-flex flex-row p-0">
+          <h2 className={`${styles.title}`}>Resultados de busqueda para: {query}</h2>
+        </div>
+      </div>
+      <div className={`${styles.resultsContainer} container`}>{isLoading ? <p>Cargando...</p> : <MovieList movies={results} mode="search" />}</div>
+    </section>
+  );
+}
