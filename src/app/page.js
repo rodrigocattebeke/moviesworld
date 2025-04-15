39b5b1fb34af1859b14fc61735328ac0bac92e50
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-04-15 16:31:59 -0300

    Refactor(home page): add hero component

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import { MovieCarousel } from "@/components/movie/MovieCarousel/MovieCarousel";
 export default async function Home() {
   return (
     <>
-      {/* <Hero /> */}
+      <Hero />
       <MovieCarousel title="Peliculas populares" route="peliculas/populares" />
       <MovieCarousel title="Mejores valoradas" route="peliculas/mejores_valoradas" />
     </>
