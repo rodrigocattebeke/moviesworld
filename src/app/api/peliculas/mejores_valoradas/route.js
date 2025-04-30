da87665430b772110a87adae9634262d7cae82d2
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-04-30 16:27:36 -0300

    refactor(api/peliculas/mejores_valoradas): change logic for detect page and sort_by params

--- src/app/api/peliculas/mejores_valoradas/route.js
+++ src/app/api/peliculas/mejores_valoradas/route.js
@@ -1,8 +1,16 @@
 import { fetchFromTMDB } from "@/lib/fetchFromTMDB";
 
 export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const params = new URLSearchParams(searchParams);
+
+  const paramsObject = {
+    page: params.get("page") || 1,
+    sort_by: params.get("sort_by") || "vote_average.desc",
+  };
+
   try {
-    const res = await fetchFromTMDB("/movie/top_rated");
+    const res = await fetchFromTMDB("/discover/movie", `${new URLSearchParams(paramsObject).toString()}`);
     return Response.json(res, { status: 200 });
   } catch (error) {
     return Response.json({ error: error.message || "Ocurrio un error en el servidor" }, { status: 500 });
--- src/app/peliculas/mejores_valoradas/page.jsx
+++ src/app/peliculas/mejores_valoradas/page.jsx
@@ -1,5 +1,5 @@
 import { MoviesPage } from "@/components/MoviesPage/MoviesPage";
 
 export default async function MejoresValoradas() {
-  return <MoviesPage title="Mejores Valoradas" url={`/api/peliculas/mejores_valoradas`} />;
+  return <MoviesPage title="Mejores Valoradas" url={`/api/peliculas/mejores_valoradas`} sectionFilter={"vote_average"} />;
 }
