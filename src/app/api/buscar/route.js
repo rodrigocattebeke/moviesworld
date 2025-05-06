133fba3c7c876ef77ac74b08de8cd86eacba4d5f
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-05-06 17:44:06 -0300

    refactor(api/buscar): add type param

--- src/app/api/buscar/route.js
+++ src/app/api/buscar/route.js
@@ -1,12 +1,12 @@
 import { fetchFromTMDB } from "@/lib/fetchFromTMDB";
-import { moviesArray } from "../moviesArray";
 
 export async function GET(req) {
   const query = req.nextUrl.searchParams.get("q");
   const page = req.nextUrl.searchParams.get("page");
+  const type = req.nextUrl.searchParams.get("type");
 
   try {
-    const res = await fetchFromTMDB("/search/movie", `include_adult=true&query=${query}&page=${page || 1}`);
+    const res = await fetchFromTMDB(`/search/${type}`, `include_adult=true&query=${query}&page=${page || 1}`);
     return Response.json(res, { status: 200 });
   } catch (error) {
     return Response.json({ error: error.message || "Ocurrio un error en el servidor" }, { status: 500 });
