ec3ae3769476ad3b5bd2fbdc4648e7355493fe6b
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-05-09 18:04:55 -0300

    feture: create app/serie page

---
+++ src/app/serie/[slug]/page.jsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { notFound, useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Loader } from "@/components/Loader/Loader";
+import { MovieCarousel } from "@/components/movie/MovieCarousel/MovieCarousel";
+import { SerieInformationView } from "@/components/serie/SerieInformationView/SerieInformationView";
+
+export default function Serie() {
+  const { slug } = useParams();
+  const [serie, setSerie] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  if (!slug) return notFound();
+
+  useEffect(() => {
+    const getResults = async () => {
+      setIsLoading(true);
+      try {
+        const id_serie = slug.split("-").pop();
+        const res = await fetch(`/api/serie?id_serie=${id_serie}`);
+        const result = await res.json();
+        setSerie(result);
+      } catch (error) {
+        console.error(error);
+        setSerie(null);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getResults();
+  }, [slug]);
+
+  return (
+    <div className="container-xxl p-0" style={{ minHeight: "30vh" }}>
+      {isLoading ? (
+        <div className="container mt-3">
+          <Loader />
+        </div>
+      ) : !serie ? (
+        <h3>Ocurrio un error al cargar la película.</h3>
+      ) : (
+        <>
+          <SerieInformationView serie={serie} />
+          {/* <MovieCarousel title={"Recomendaciones"} route="peliculas/populares" /> */}
+        </>
+      )}
+    </div>
+  );
+}
