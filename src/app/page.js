f5aebc6e9c026202dc9f285ba63c87d56c75330b
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-05-12 17:30:39 -0300

    refactor(app/page): change the logic for make a fetch is ssr

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,24 @@ import { Hero } from "@/components/Hero/Hero";
 import { MovieCarousel } from "@/components/movie/MovieCarousel/MovieCarousel";
 
 export default async function Home() {
-  return (
-    <>
-      <Hero />
-      <MovieCarousel title="Peliculas populares" route="peliculas/populares" />
-      <MovieCarousel title="Mejores valoradas" route="peliculas/mejores_valoradas" />
-    </>
-  );
+  try {
+    const [popularesRes, mejoresRes] = await Promise.all([fetch("https://moviesloc.netlify.app/api/peliculas/populares", { cache: "no-store" }), fetch("https://moviesloc.netlify.app/api/peliculas/mejores_valoradas", { cache: "no-store" })]);
+
+    const populares = await popularesRes.json();
+    const mejores = await mejoresRes.json();
+
+    return (
+      <>
+        {console.log(populares)}
+        <>
+          <Hero />
+          <MovieCarousel title="Peliculas populares" contentList={populares.results} type={"peliculas"} />
+          <MovieCarousel title="Mejores valoradas" contentList={mejores.results} type={"peliculas"} />
+        </>
+      </>
+    );
+  } catch (error) {
+    console.error("Error al acceder al servidor:", error);
+    return <h2 className="my-4">Error loading data...</h2>;
+  }
 }
