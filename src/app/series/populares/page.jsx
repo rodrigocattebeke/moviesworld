d22be3a29c64f1164bf7d98b8aee5989796532c0
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-05-19 17:29:04 -0300

    feat(app/series): create series/populares and mejores valoradas page

---
+++ src/app/series/populares/page.jsx
@@ -0,0 +1,11 @@
+import { ContentPage } from "@/components/ContentPage/ContentPage";
+import { Loader } from "@/components/Loader/Loader";
+import { Suspense } from "react";
+
+export default async function Popular() {
+  return (
+    <Suspense fallback={<Loader />}>
+      <ContentPage title="Populares" url={`/api/series/populares`} sectionFilter={"popularity"} type={"series"} />;
+    </Suspense>
+  );
+}
