713eda675e30496e1608c05529ea28595ea87a52
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-05-19 17:23:54 -0300

    refactor(app/peliculas):change MoviesPage to ContentPage

--- src/app/peliculas/mejores_valoradas/page.jsx
+++ src/app/peliculas/mejores_valoradas/page.jsx
@@ -1,11 +1,11 @@
+import { ContentPage } from "@/components/ContentPage/ContentPage";
 import { Loader } from "@/components/Loader/Loader";
-import { MoviesPage } from "@/components/MoviesPage/MoviesPage";
 import { Suspense } from "react";
 
 export default async function MejoresValoradas() {
   return (
     <Suspense fallback={<Loader />}>
-      <MoviesPage title="Mejores Valoradas" url={`/api/peliculas/mejores_valoradas`} sectionFilter={"vote_average"} />;
+      <ContentPage title="Mejores Valoradas" url={`/api/peliculas/mejores_valoradas`} sectionFilter={"vote_average"} type={"peliculas"} />;
     </Suspense>
   );
 }
--- src/app/peliculas/populares/page.jsx
+++ src/app/peliculas/populares/page.jsx
@@ -1,11 +1,11 @@
+import { ContentPage } from "@/components/ContentPage/ContentPage";
 import { Loader } from "@/components/Loader/Loader";
-import { MoviesPage } from "@/components/MoviesPage/MoviesPage";
 import { Suspense } from "react";
 
 export default async function Popular() {
   return (
     <Suspense fallback={<Loader />}>
-      <MoviesPage title="Populares" url={`/api/peliculas/populares`} sectionFilter={"popularity"} />;
+      <ContentPage title="Populares" url={`/api/peliculas/populares`} sectionFilter={"popularity"} type={"peliculas"} />;
     </Suspense>
   );
 }
