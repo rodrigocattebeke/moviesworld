83c5645796dba61a3805a0252e99d25e721d2281
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-05-21 18:21:21 -0300

    refactor(Header): add moviesloc logo with href for return to the home page

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -12,10 +12,15 @@ export const Header = () => {
       <nav className={`${styles.navbar} container-xxl`}>
         <div className="container-fluid h-100">
           <div className="row h-100 m-0">
-            <div className={`${styles.searchContainer} col-9 col-md-8 col-xl-5`}>
-              <SearchInput />
+            <div className={`d-flex col-9 col-md-9 col-xl-5`}>
+              <Link href={"/"}>
+                <Image src={"/assets/images/logo-short-form.png"} alt="Logo de MoviesLoc" width={32} height={32}></Image>
+              </Link>
+              <div className={styles.searchContainer}>
+                <SearchInput />
+              </div>
             </div>
-            <div className="col-3 col-md-4 col-xl-7 d-flex align-items-center">
+            <div className="col-3 col-md-3 col-xl-7 d-flex align-items-center">
               <div className="d-flex justify-content-end justify-content-xl-center w-100 h-100">
                 <div className="d-block d-xl-none">
                   <a data-bs-toggle="offcanvas" data-bs-target="#headerOffcanvasMenu" role="button" aria-controls="headerOffcanvasMenu">
