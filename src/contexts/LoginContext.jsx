a5216fc0bcd84d9762406863f1b16880d19e03be
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-05-29 17:04:18 -0300

    feat(LoginContext): create register function

--- src/contexts/LoginContext.jsx
+++ src/contexts/LoginContext.jsx
@@ -3,10 +3,17 @@ import { useRouter } from "next/navigation";
 import { createContext, useEffect, useState } from "react";
 
 const LoginContext = createContext();
+const userBasicInformation = {
+  user: undefined,
+  favorites: {
+    movies: undefined,
+    series: undefined,
+  },
+};
 
 export const LoginProvider = ({ children }) => {
-  const [userData, setUserData] = useState(undefined);
-  const isLogged = !!userData;
+  const [loggedUserData, setLoggedUserData] = useState(undefined);
+  const isLogged = !!loggedUserData;
   const [usersDB, setUsersDB] = useState(undefined);
   const navigate = useRouter();
 
@@ -19,12 +26,12 @@ export const LoginProvider = ({ children }) => {
   //If the localstorage have a saved user logged, find it.
   useEffect(() => {
     if (!usersDB) return;
-    const savedUserLogged = localStorage.getItem("MLLoggedUser") || undefined;
+    const savedLoggedUser = localStorage.getItem("MLLoggedUser") || undefined;
 
-    if (savedUserLogged) {
-      if (usersDB.hasOwnProperty(savedUserLogged)) {
-        const { password, ...publicUserData } = usersDB[savedUserLogged];
-        setUserData(publicUserData);
+    if (savedLoggedUser) {
+      if (usersDB.hasOwnProperty(savedLoggedUser)) {
+        const { password, ...publicUserData } = usersDB[savedLoggedUser];
+        setLoggedUserData(publicUserData);
       }
     }
   }, [usersDB]);
@@ -35,33 +42,51 @@ export const LoginProvider = ({ children }) => {
     setUsersDB(usersDB);
   };
 
+  const register = (user, password) => {
+    if (usersDB.hasOwnProperty(user)) {
+      return {
+        isSuccess: false,
+        errorMessage: "El nombre de usuario ya existe. Intenta con otro nombre de usuario.",
+      };
+    }
+
+    const usersDBCloned = usersDB;
+
+    usersDBCloned[user] = {
+      ...userBasicInformation,
+      user,
+      password,
+    };
+
+    localStorage.setItem("MLUsersDB", JSON.stringify(usersDBCloned));
+    localStorage.setItem("MLLoggedUser", user);
+    setLoggedUserData({ ...userBasicInformation, user });
+    refreshUsers();
+    navigate.push("./");
+    return { isSuccess: true, errorMessage: undefined };
+  };
+
   //Login
   const login = (user, password) => {
     refreshUsers();
-    let loginRes = false;
-
-    if (usersDB.hasOwnProperty(user)) {
-      if (usersDB[user].password && usersDB[user].password === password) {
-        localStorage.setItem("MLLoggedUser", user);
-        navigate.push("./");
-        return (loginRes = true);
-      } else {
-        return (loginRes = false);
-      }
+    if (usersDB.hasOwnProperty(user) && usersDB[user].password === password) {
+      localStorage.setItem("MLLoggedUser", user);
+      navigate.push("./");
+      return true;
     } else {
-      return (loginRes = false);
+      return false;
     }
   };
 
   //Log out
   const logout = () => {
-    if (userData) {
-      setUserData(undefined);
+    if (loggedUserData) {
+      setLoggedUserData(undefined);
       localStorage.removeItem("MLLoggedUser");
     }
   };
 
-  return <LoginContext.Provider value={{ isLogged, login, logout, userData }}>{children}</LoginContext.Provider>;
+  return <LoginContext.Provider value={{ isLogged, login, logout, loggedUserData, register }}>{children}</LoginContext.Provider>;
 };
 
 export { LoginContext };
