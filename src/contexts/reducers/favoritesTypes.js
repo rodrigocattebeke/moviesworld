12e1a1960b3f4a0cc717d5437263f53fc9516868
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-05-30 18:16:27 -0300

    feat(favoriteReducer):_ create reducer for manage favorites in user account

---
+++ src/contexts/reducers/favoritesTypes.js
@@ -0,0 +1,5 @@
+export const SYNC_FAVORITES = "SYNC_FAVORITES";
+export const ADD_MOVIE = "ADD_MOVIE";
+export const REMOVE_MOVIE = "REMOVE_MOVIE";
+export const ADD_SERIE = "ADD_SERIE";
+export const REMOVE_SERIE = "REMOVE_SERIE";
