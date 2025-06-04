5107ba869af65e156a5915dd50d63a0d21eb4480
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-06-04 16:52:07 -0300

    feat(MovieCard): add to moviecard the AddToFavorite component

--- src/components/movie/MovieCard/MovieCard.jsx
+++ src/components/movie/MovieCard/MovieCard.jsx
@@ -5,21 +5,29 @@ import { MovieRate } from "./MovieRate/MovieRate";
 import { getPosterUrl } from "@/utils/getPosterUrl";
 import Link from "next/link";
 import { titleToSlug } from "@/utils/titleToSlug";
+import { AddToFavorite } from "@/components/AddToFavorite/AddToFavorite";
 
 export const MovieCard = ({ movie = undefined, mode = "", type = null }) => {
   if (!movie) return console.error("Se debe de pasar una pelicula o serie.");
-  if (!type) return console.error("Se debe de pasar un tipo. Disponibles: Series, Peliculas");
+  if (!type) return console.error("Se debe de pasar un tipo. Disponibles: serie, pelicula");
+  if (type !== "serie" && type !== "pelicula") return console.error("El tipo pasado es incorrecto. Tipos validos: serie - pelicula");
+
   //Normalize
-  const title = type == "series" ? movie.name : movie.title;
+  const title = type == "serie" ? movie.name : movie.title;
   const slug = titleToSlug(title);
-  const url = `/${type == "series" ? "serie" : "pelicula"}/${slug}-${movie.id}`;
+  const url = `/${type == "serie" ? "serie" : "pelicula"}/${slug}-${movie.id}`;
   const imageUrl = getPosterUrl(movie.poster_path);
   return (
-    <Link href={url}>
-      <div className={`${styles.cardContainer} ${mode == "search" ? styles.searchResultCard : ""}`}>
+    <div className={`${styles.cardContainer} ${mode == "search" ? styles.searchResultCard : ""}`}>
+      <div className={styles.addToFavoritesContainer}>
+        <AddToFavorite type={type} id={movie.id} />
+      </div>
+      <Link href={url}>
         <div className={`${styles.cardImgContainer}`}>
           <Image src={imageUrl} width={150} height={225} alt={`Poster de la película ${title}`} sizes="(max-width: 380px) 80vw, (max-width: 540px) 45vw, (max-width:750px) 33vw, (max-width: 940px) 24vw, (max-width: 1200px) 20vw, 15vw" />
         </div>
+      </Link>
+      <Link href={url}>
         <div className={`${mode == "search" ? styles.hideMovieTitle : "d-none"}`}>
           <p>{title}</p>
         </div>
@@ -27,7 +35,7 @@ export const MovieCard = ({ movie = undefined, mode = "", type = null }) => {
           <p className={`${styles.movieTitle}`}>{title}</p>
           <MovieRate rate={movie.vote_average} />
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
