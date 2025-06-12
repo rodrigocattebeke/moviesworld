b1b0fb0b2adb3bdb06467b58f1bea3b0a73d6cef
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-06-12 16:45:49 -0300

    feat(Footer): modify footer and add link to terms and conditions page

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,14 +1,20 @@
+import Link from "next/link";
+import styles from "./Footer.module.css";
+
 export const Footer = () => {
   return (
-    <footer className="container-fluid">
-      <div className="container-xxl">
+    <footer className={styles.container}>
+      <div className="container">
         <p>
           Datos proveidos por{" "}
           <a href="https://www.themoviedb.org" target="_blank" rel="noopener noreferrer">
             TMDb
           </a>
         </p>
-        <p>This website uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by TMDB.</p>
+        <p>Este sitio web utiliza TMDB y las API de TMDB, pero no está respaldado, certificado ni aprobado de otro modo por TMDB.</p>
+        <p>
+          Todo el contenido disponible está vinculado o incrustado desde plataformas de terceros con fines informativos. El uso del sitio implica la aceptación de nuestros <Link href={"/terminos-y-condiciones"}>términos y condiciones.</Link>
+        </p>
       </div>
     </footer>
   );
