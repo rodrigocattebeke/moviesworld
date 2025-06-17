2817e4e9251e20e6c527d45a5afba6ac7fc2a1b1
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-06-17 16:20:55 -0300

    feat(pages): create movie page and serie page view

---
+++ src/components/pages/MoviePage/MoviePage.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { notFound, useParams } from "next/navigation";
+import { ContentInformationView } from "@/components/content/ContentInformationView/ContentInformationView";
+import { Loader } from "@/components/Loader/Loader";
+import { ContentCarousel } from "@/components/movie/ContentCarousel/ContentCarousel";
+import useFetch from "@/hooks/useFetch";
+
+export const MoviePage = () => {
+  const { slug } = useParams();
+  const id_pelicula = slug.split("-").pop();
+
+  const { data: movie, isLoading: movieLoading } = useFetch(`/api/pelicula?id_pelicula=${id_pelicula}`);
+  const { data: recomendation, isLoading: recomendationLoading } = useFetch(`/api/pelicula/recomendaciones/${id_pelicula}`);
+  if (!slug) return notFound();
+  return (
+    <div className="container-xxl p-0" style={{ minHeight: "30vh" }}>
+      {movieLoading || recomendationLoading ? (
+        <div className="container mt-3">
+          <Loader />
+        </div>
+      ) : !movie || !recomendation ? (
+        <h3>Ocurrio un error al cargar la película.</h3>
+      ) : (
+        <>
+          <ContentInformationView type={"pelicula"} content={movie} />
+          {recomendation.results.length == 0 ? "" : <ContentCarousel title={"Recomendaciones"} contentList={recomendation.results} type={"peliculas"} />}
+        </>
+      )}
+    </div>
+  );
+};
