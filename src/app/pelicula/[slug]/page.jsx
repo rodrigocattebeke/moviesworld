143ae72f799728e63953237d06712a2411970254
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-06-17 16:33:01 -0300

    feat: extract the app/serie and app/pelicula logic to a separate component for a better structure

--- src/app/pelicula/[slug]/page.jsx
+++ src/app/pelicula/[slug]/page.jsx
@@ -1,32 +1,15 @@
-"use client";
+import { MoviePage } from "@/components/pages/MoviePage/MoviePage";
 
-import { notFound, useParams } from "next/navigation";
-import { ContentInformationView } from "@/components/content/ContentInformationView/ContentInformationView";
-import { Loader } from "@/components/Loader/Loader";
-import { ContentCarousel } from "@/components/movie/ContentCarousel/ContentCarousel";
-import useFetch from "@/hooks/useFetch";
+export async function generateMetadata({ params }) {
+  const slug = (await params).slug;
+  const title = slug.split("-")[0];
+
+  return {
+    title: `${title} - MoviesLoc`,
+    description: `Conocé todo sobre ${title} en MoviesLoc.`,
+  };
+}
 
 export default function Pelicula() {
-  const { slug } = useParams();
-  const id_pelicula = slug.split("-").pop();
-
-  const { data: movie, isLoading: movieLoading } = useFetch(`/api/pelicula?id_pelicula=${id_pelicula}`);
-  const { data: recomendation, isLoading: recomendationLoading } = useFetch(`/api/pelicula/recomendaciones/${id_pelicula}`);
-  if (!slug) return notFound();
-  return (
-    <div className="container-xxl p-0" style={{ minHeight: "30vh" }}>
-      {movieLoading || recomendationLoading ? (
-        <div className="container mt-3">
-          <Loader />
-        </div>
-      ) : !movie || !recomendation ? (
-        <h3>Ocurrio un error al cargar la película.</h3>
-      ) : (
-        <>
-          <ContentInformationView type={"pelicula"} content={movie} />
-          {recomendation.results.length == 0 ? "" : <ContentCarousel title={"Recomendaciones"} contentList={recomendation.results} type={"peliculas"} />}
-        </>
-      )}
-    </div>
-  );
+  return <MoviePage />;
 }
--- src/app/serie/[slug]/page.jsx
+++ src/app/serie/[slug]/page.jsx
@@ -1,37 +1,15 @@
-"use client";
+import { SeriePage } from "@/components/pages/SeriePage/SeriePage";
 
-import { notFound, useParams } from "next/navigation";
-import { Loader } from "@/components/Loader/Loader";
-import { SeasonsCarousel } from "@/components/serie/SeasonsCarousel/SeasonsCarousel";
-import useFetch from "@/hooks/useFetch";
-import { ContentCarousel } from "@/components/movie/ContentCarousel/ContentCarousel";
-import { ContentInformationView } from "@/components/content/ContentInformationView/ContentInformationView";
+export async function generateMetadata({ params }) {
+  const slug = (await params).slug;
+  const title = slug.split("-")[0];
+
+  return {
+    title: `${title} - MoviesLoc`,
+    description: `Conocé todo sobre ${title} en MoviesLoc.`,
+  };
+}
 
 export default function Serie() {
-  const { slug } = useParams();
-
-  const id_serie = slug.split("-").pop();
-
-  const { data: serie, isLoading: serieLoading } = useFetch(`/api/serie?id_serie=${id_serie}`);
-  const { data: recomendation, isLoading: recomendationLoading } = useFetch(`/api/serie/recomendaciones/${id_serie}`);
-
-  if (!slug) return notFound();
-
-  return (
-    <div className="container-xxl p-0" style={{ minHeight: "30vh" }}>
-      {serieLoading || recomendationLoading ? (
-        <div className="container mt-3">
-          <Loader />
-        </div>
-      ) : !serie || !recomendation ? (
-        <h3 className="my-4">Ocurrio un error al cargar la película.</h3>
-      ) : (
-        <>
-          <ContentInformationView type={"serie"} content={serie} />
-          <SeasonsCarousel seasons={serie.seasons} />
-          {recomendation.results.length == 0 ? "" : <ContentCarousel title={"Recomendaciones"} contentList={recomendation.results} type={"series"} />}
-        </>
-      )}
-    </div>
-  );
+  return <SeriePage />;
 }
